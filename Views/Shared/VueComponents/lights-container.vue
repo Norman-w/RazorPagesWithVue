c312af5d42398f3411701d36dd47ec825c4ff9fc
Norman <85535885@qq.com>
2023-09-06 06:58:05 +0800

    从cshtml中给vue组件传递默认值的方式

--- Views/Shared/VueComponents/lights-container.vue
+++ Views/Shared/VueComponents/lights-container.vue
@@ -1,5 +1,6 @@
 <template type="text/html" id="lights-container">
-  <div class="light-group">
+<!--  动态使用props的border颜色-->
+  <div class="light-group" :style="{borderColor:color}">
     <sun :color="allOn ? 'red' : 'yellow'" v-on:on-sun-click="onLightClick"></sun>
     <light v-for="i in 3" :key="i" :allOn="allOn" v-on:on-light-click="onLightClick" ></light>
     <small-light></small-light>
@@ -13,6 +14,15 @@ import light from "./light.vue";
 import sun from "./sun.vue";
 let comp = {
   template: "#" + "lights-container",
+  // 这种可以
+  // props: ["color"],
+  // 这种也可以
+  props: {
+    color: {
+      type: String,
+      default: "blue"
+    }
+  },
   components: {
     smallLight:smallLight,
     light:light,
@@ -31,6 +41,10 @@ let comp = {
       console.log("light clicked");
       this.allOn = false;
     }
+  },
+  mounted() {
+    console.log("组件sun加载完成");
+    console.log("props传递的颜色值:",this.color);
   }
 }
 Vue.component("lights-container", comp);
